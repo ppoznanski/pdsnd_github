35c6f197ad8a20f9a3ae15d624481870b25552a4
Piotr Poznanski <piotr.poznanski@gmail.com>
2019-05-15 22:50:47 +0200

    Added comment in project_2_example.py on data imported by the script.

--- project_2_example.py
+++ project_2_example.py
@@ -1,6 +1,9 @@
 import time
 import pandas as pd
 import numpy as np
+"""
+Files including historical bike shares from 3 cities.
+"""
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
